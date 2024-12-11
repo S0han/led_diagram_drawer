9579b1eb517d963d655f9e3c70cd279990d13fe3
S0han <kane.pereira18@gmail.com>
2024-12-11 12:55:29 -0500

    update: setup configration component on main page

--- src/app/components/Configuration.component.tsx
+++ src/app/components/Configuration.component.tsx
@@ -1,5 +1,13 @@
+import DropDown from './DropDown.component';
+import ToggleBtn from './ToggleBtn.component';
+import Inputfield from './InputField.component';
+
 export default function Configuration() {
     return (
-        <></>
+        <div className='configuration-box'>
+            <DropDown />
+            <ToggleBtn options={["btn1", "btn2"]} />
+            <Inputfield labelName={"Floor Distance"} />
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 'use client'
-import DropDown from './components/DropDown.component';
-import ToggleBtn from './components/ToggleBtn.component';
-import Inputfield from './components/InputField.component';
+import Configuration from './components/Configuration.component';
 
 export default function Home() {
   return (
     <div>
-      <DropDown />
-      <ToggleBtn options={["btn1", "btn2"]} />
-      <Inputfield labelName={"Floor Distance"} />
+      <Configuration />
     </div>
   )
 }

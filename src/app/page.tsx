6b2c97517b596600dd042d5d9c04169d03977133
S0han <kane.pereira18@gmail.com>
2024-12-11 15:12:07 -0500

    update: create a function to fetch CSV data from a local file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
+import React, { useState } from 'react';
 import Configuration from './components/Configuration.component';
+import Papa from 'papaparse';
 
 export default function Home() {
   return (

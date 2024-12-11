8d45f58023dacd061715647116e57d1ff2407510
S0han <kane.pereira18@gmail.com>
2024-12-11 16:05:13 -0500

    update: add logic to fetchCSVData function

--- src/app/utils/fetchCSVData.tsx
+++ src/app/utils/fetchCSVData.tsx
@@ -1,3 +1,25 @@
-export default function fetchCSVData() {
-    
-}
+import Papa from 'papaparse';
+import React from 'react';
+
+export default function fetchCSVData({ onFileLoad }){
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+
+        if (file) {
+            Papa.parse(file, {
+                complete: (result) => {
+                  onFileLoad(result.data);  
+                },
+                header: true,
+                dynamicTyping: true,
+                skipEmptyLines: true,
+            });
+        }
+    };
+
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+        </div>
+    )
+};   

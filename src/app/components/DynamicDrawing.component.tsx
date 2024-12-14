b10abaa8c2b4496cbc6e3b1b81f56d9187191d6c
S0han <kane.pereira18@gmail.com>
2024-12-14 18:05:55 -0500

    update: add container in drwaing box to hold dynamic scketch

--- src/app/components/DynamicDrawing.component.tsx
+++ src/app/components/DynamicDrawing.component.tsx
@@ -1,7 +1,10 @@
-export default function DynamicDrawing() {
+export default function DynamicDrawing({ height, width, floorDistance }) {
+    const SVG_SIZE = 600;    
+    
+    const scalingFactor = Math.max(width, height, floorDistance) > 0
+        ? SVG_SIZE / Math.max(width, height, floorDistance) : 1;
+
     return (
-        <div>
-            <p>DYNAMIC DRAWING</p>
-        </div>
+        <svg width={SVG_SIZE} height={SVG_SIZE} style={{ border: "1px solid black" }}></svg>
     )
 }

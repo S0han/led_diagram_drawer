73dbde85459d414ec97cd418a071ae6c4772b719
S0han <kane.pereira18@gmail.com>
2024-12-15 12:25:24 -0500

    update: correctly layout the screen componenets

--- src/app/components/PrintableArea.component.tsx
+++ src/app/components/PrintableArea.component.tsx
@@ -6,22 +6,22 @@ export default function PrintableArea({ dropDownData, selectedModel, selectedFlo
     const selectedData = dropDownData.find(item => item.Screen_MFR === selectedModel);
     
     return(
-        <div className="printable-sheet">
-            <div className="drawing-side">
+        <div className="flex w-full h-[calc(100vh-4rem)]">
+            <div className="flex-1 p-4 border-r">
                 <DynamicDrawing 
                     height={selectedData?.Height} 
                     width={selectedData?.Width} 
                     floorDistance={selectedFloorDistance}
                 />
             </div>
-            <div className="non-drawing-side">
-                <div>
+            <div className="flex-1 flex flex-col justify-between p-4">
+                <div className="bg-blue-100 p-2">
                     <ScreenDimensions 
                         flrDistanceData={selectedFloorDistance} 
                         screenData={selectedData}
                     />
                 </div>
-                <div>
+                <div className="bg-green-100 p-2">
                     <DrawingInfo description={description}/>
                 </div>
             </div>

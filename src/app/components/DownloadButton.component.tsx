41bb556cabb70a715c37b933ff4506628cf4e5d2
S0han <kane.pereira18@gmail.com>
2024-12-15 17:06:24 -0500

    update: styling button

--- src/app/components/DownloadButton.component.tsx
+++ src/app/components/DownloadButton.component.tsx
@@ -1,6 +1,6 @@
 export default function DownloadButton() {
     return (
-        <div>
+        <div className="bg-red-500 hover:bg-red-700 text-white w-full text-center py-2 rounded-md">
             <button>DOWNLOAD!</button>
         </div>
     )

de0daf959aa3e84250fee919d3a114e106a1ccf6
S0han <kane.pereira18@gmail.com>
2024-12-14 19:53:34 -0500

    update: drawing dynamically displays screen dimensions

--- src/app/components/DynamicDrawing.component.tsx
+++ src/app/components/DynamicDrawing.component.tsx
@@ -1,10 +1,64 @@
-export default function DynamicDrawing({ height, width, floorDistance }) {
-    const SVG_SIZE = 600;    
-    
-    const scalingFactor = Math.max(width, height, floorDistance) > 0
-        ? SVG_SIZE / Math.max(width, height, floorDistance) : 1;
+export default function DynamicDrawing({ height = 0, width = 0, floorDistance = 0 }) {
+    const SVG_SIZE = 600;
+    const rectScalingFactor = 0.6;
+    const minRectHeight = 250;
+    const scaledHeight = Math.max(height * rectScalingFactor, minRectHeight);
+    const scaledWidth = SVG_SIZE * rectScalingFactor;
+    const scaledFloorDistance = floorDistance > 0 ? floorDistance : 0;
 
     return (
-        <svg width={SVG_SIZE} height={SVG_SIZE} style={{ border: "1px solid black" }}></svg>
-    )
+        <svg width={SVG_SIZE} height={SVG_SIZE} style={{ border: "1px solid black" }}>
+            <rect 
+                x={(SVG_SIZE - scaledWidth) / 2}
+                y={50}
+                width={scaledWidth}
+                height={scaledHeight}
+                fill="white"
+                stroke="black"
+                strokeWidth="2"
+            />
+            <text 
+                x={(SVG_SIZE - scaledWidth) / 2} 
+                y={40}
+                textAnchor="start" 
+                alignmentBaseline="middle"
+                fontSize="16"
+                fill="black"
+            >
+                Width: {width}"
+            </text>
+            <text 
+                x={((SVG_SIZE - scaledWidth) / 2 + scaledWidth) - 5} 
+                y={scaledHeight / 2 + 50}
+                textAnchor="end" 
+                alignmentBaseline="middle"
+                fontSize="16"
+                fill="black"
+            >
+                Height: {height}"
+            </text>
+            {scaledFloorDistance > 0 && (
+                <>
+                    <line 
+                        x1={SVG_SIZE / 2}
+                        y1={50 + scaledHeight} 
+                        x2={SVG_SIZE / 2}
+                        y2={SVG_SIZE} 
+                        stroke="black"
+                        strokeWidth="1"
+                    />
+                    <text 
+                        x={SVG_SIZE / 2 + 5} 
+                        y={(50 + scaledHeight + SVG_SIZE) / 2}
+                        textAnchor="start" 
+                        alignmentBaseline="middle"
+                        fontSize="16"
+                        fill="black"
+                    >
+                        Floor Distance: {scaledFloorDistance}"
+                    </text>
+                </>
+            )}
+        </svg>
+    );
 }
